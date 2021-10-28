e5549565b2b20282922216896d07f53b2d3b71c4
SandyBridge_user <37615664+sandybridge-user@users.noreply.github.com>
2021-10-28 22:39:23 +0900

    Add files via upload

--- Blink.c
+++ Blink.c
@@ -22,72 +22,75 @@
 int led = 0;
 int pin = 0;
 int pin_num = 0;
+int i = 0;
 
 int main(void) {
 
 	//Blink.inoを作成
-	FILE* fp;
-	fp = fopen("Blink.ino", "a"); //Blink.inoを書き込みモードwで開く。
-	fprintf(fp, "void setup() {\n");
-	//fclose(fp);
+	FILE *fp;
+	fp = fopen("Blink.ino", "a"); //Blink.inoを書き込みモードaで開く。
+		//void setup(){ を入力
+		fprintf(fp, "void setup() {\n");
+		fprintf(fp, "//以下が接続するピンの設定です。\n");
 
 
 	printf("このプログラムはArduino Uno又はその互換ボード用です。他のボードには使えません。\n");
 
 	//LEDの個数を質問
 	printf("点滅させるLEDの個数(自然数)を入力して、Enterキーを押してください。\n");
-
 	scanf("%d", &led);
 	printf("入力された個数:%d \n", led);
+	printf("\n");
 
        
 	//接続するPINを質問
-        //fp = fopen("Blink.ino", "a"); //追加書き込み専用で開く
-	for (int i = 0 ; i < led; i++) {
+	while (i < led) {
 		printf("接続するピン番号(自然数)を入力してください。\n");
 		scanf("%d", &pin_num);
 		
 		fprintf(fp, "pinMode(%d, OUTPUT);\n", pin_num);
-		//fclose(fp);
 		printf("接続するピン：%d をOUTPUTに設定\n" ,pin_num);
+		printf("\n");
+		i++;
 	}
-        //fclose(fp);
-	//fp = fopen("Blink.ino", "a"); //追加書き込み専用で開く
-	fprintf(fp, "}\n");
-	fprintf(fp, "void loop(){\n");
-	//fclose(fp);
 
+	fprintf(fp, "}\n");
+	fprintf(fp, "\n");
+	fprintf(fp, "void loop(){\n");
 	
 	//LEDを点滅させる順番を質問
 	printf("\n");
 	printf("LEDを接続するピンを決めましょう。\n");
 
-	for (int i = 0; i < led; i++) {
-		//質問
+	i = 0;
+
+	while (i < led) {
+		//点灯させる順番を質問
 		printf("%d回目に点灯させるLEDのPIN番号を入力してください。\n", i + 1);
-		scanf("%d", pin);
+		scanf("%d", &pin);
 		printf("%d番を点灯させます。\n", pin);
 		
 		printf("\n");
 		printf("何ミリ秒点灯させますか？\n");
 		int wait_t = 0;
-		scanf("%d", wait_t);
+		scanf("%d", &wait_t);
 		printf("%dミリ秒間点灯\n", wait_t);
+		printf("\n");
 
 		//書き込み
-		//fp = fopen("Blink.ino", "a"); //追加書き込み専用で開く
 		fprintf(fp, "digitalWrite(%d, HIGH);\n", pin);
-		fprintf(fp, "delay(%d)\n" , wait_t);
+		fprintf(fp, "delay(%d);\n" , wait_t);
 		fprintf(fp, "digitalWrite(%d, LOW);\n", pin);
-		//fclose(fp);
+		fprintf(fp, "\n");
+		i++;
 	
 		}
 	
-	//fp = fopen("Blink.ino", "a"); //追加書き込み専用で開く
 	fprintf(fp, "}\n");
 	fclose(fp);
 
 	printf("質問は以上です。\n");
+	printf("プログラムが出来上がりました。\n");
 	
 
 	return 0;
